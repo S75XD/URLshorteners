43d575b75e5236b92c7916c288b3a7b174eb1219
S75XD <66068313+S75XD@users.noreply.github.com>
2021-05-26 17:41:28 +0300

    Fix a Bug

--- URLshortener.py
+++ URLshortener.py
@@ -11,15 +11,15 @@ To EXIT [4]
 """)
 while True:
     Text = input(":")
-    if Text == 1:
+    if Text == '1':
         Link = input("URL:")
         s = pyshorteners.Shortener()
         print(s.tinyurl.short(Link))
-    elif Text == 2:
+    elif Text == '2':
         webbrowser.open("https://twitter.com/S75XD")
-    elif Text == 3:
+    elif Text == '3':
         webbrowser.open("https://twitter.com/F14Commander")
-    elif Text == 4:
+    elif Text == '4':
         exit("Ok")
     else:
         print("Error")
